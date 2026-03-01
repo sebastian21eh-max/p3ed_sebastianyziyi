a3090a3efc75f12876745514d66baddccb8504a9
Sebastian <sebastian21@gmail.com>
2026-03-01 22:39:18 +0100

    tercer commit - nuevo comentario

--- PruebaPersona/src/pruebapersona/persona.java
+++ PruebaPersona/src/pruebapersona/persona.java
@@ -1,5 +1,29 @@
-package pruebapersona;
+// COMENTARIO AÑADIDO (MODIFICACIÓN DEL FICHERO)
 
+package pruebapersona;
+/**
+ * Clase Persona
+ * Representa una persona con nombre y edad
+ */
 public class persona {
 
+    // ===== ATRIBUTOS =====
+    String nombre;  // Guarda el nombre de la persona
+    int edad;       // Guarda la edad de la persona
+
+    // ===== MÉTODOS =====
+
+    /**
+     * Método que muestra un saludo
+     */
+    public void saludar() {
+        System.out.println("Hola, me llamo " + nombre);
+    }
+
+    /**
+     * Método que muestra la edad
+     */
+    public void mostrarEdad() {
+        System.out.println("Tengo " + edad + " años");
+    }
 }
