00916218ad59a110e3547c21904a11d7c9a39197
Sebastian <sebastian21@gmail.com>
2026-03-01 23:30:56 +0100

    me he olvidado de la clase pruebapersona

--- PruebaPersona/src/pruebapersona/PruebaPersona.java
+++ PruebaPersona/src/pruebapersona/PruebaPersona.java
@@ -1,5 +1,26 @@
 package pruebapersona;
 
+/**
+ * Clase principal
+ * Aquí se ejecuta el programa
+ */
 public class PruebaPersona {
 
+    /**
+     * Método main
+     * Es el punto de inicio del programa
+     */
+    public static void main(String[] args) {
+
+        // ===== CREACIÓN DEL OBJETO =====
+        persona p1 = new persona(); // Se crea un objeto de tipo Persona
+
+        // ===== ASIGNACIÓN DE VALORES =====
+        p1.nombre = "Ana";
+        p1.edad = 20;
+
+        // ===== LLAMADA A MÉTODOS =====
+        p1.saludar();
+        p1.mostrarEdad();
+    }
 }
